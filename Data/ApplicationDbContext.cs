ac40dd902c7b405d05bd76e8edf6aa78a2b1b75e
trung-hao-tran <trunghao.tran.2000@gmail.com>
2024-11-13 03:29:33 +1100

    #4 Add renter model, database, and seed data
    Add dbContext, db connection string, mitigration files, renter model, seed data.
    Note that, renter modal might change, and no views or controller are using database right now

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalManagementSystem.Models;
+
+namespace RentalManagementSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<RenterModel> Renters { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<RenterModel>().HasData(
+                new RenterModel { Id= 1, Name ="Hao"},
+                new RenterModel { Id = 2, Name = "Hoa"},
+                new RenterModel { Id = 3, Name = "Hai" }
+            );
+        }
+    }
+}
---
+++ Models/RenterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RentalManagementSystem.Models
+{
+    public class RenterModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+    }
+}
