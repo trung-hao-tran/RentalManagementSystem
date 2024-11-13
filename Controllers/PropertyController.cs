40cf2bf42e62d4ae95a7f511ee3c0868514701ec
trung-hao-tran <trunghao.tran.2000@gmail.com>
2024-11-14 03:38:51 +1100

    #2 Add list of renters table. edit/delete option is available in actions column
    Add property controllers and property view. The view has tables of renters. data is retrieved with db context.
    Note: edit and delete is not working, Need an issue to add edit/ delte controller/actions/view

---
+++ Controllers/PropertyController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentalManagementSystem.Data;
+
+namespace RentalManagementSystem.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PropertyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var allRenters = _context.Renters.ToList();
+            return View(allRenters);
+        }
+    }
+}
