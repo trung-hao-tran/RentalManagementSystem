e028581abd05971557fe6a07ded1130ca9883787
trung-hao-tran <trunghao.tran.2000@gmail.com>
2024-11-14 03:50:10 +1100

    #3 Add a buttons for adding renter
    Add a button for adding renters
    Note: the button is not working. actions/view/controller will be done in #5

---
+++ Models/PropertyModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace RentalManagementSystem.Models
+{
+    public class PropertyModel
+    {
+
+        public int Id { get; set; }
+        public required decimal Price { get; set; }
+        public required string Number { get; set; }
+        public List<RenterModel> Renters { get; set; } = [];
+    }
+}
